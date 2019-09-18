043f390d499c7b556feca6f9eec9423caaf27489
Vivet Anthony (HES) <anthony.vivet@hes.adhes.hesge.ch>
2019-09-18 17:51:09 +0200

    ma première sauvegarde

---
+++ bonjour.py
@@ -0,0 +1,12 @@
+"""bienvenue
+
+données : le semestre d'entrée
+résultat : affiche un message de bienvenue aux étudiant-e-s
+"""
+
+
+### déclaration des variables
+### initialisation des variables
+### séquence d'opératons
+print("Bonjour !")
+print("Bienvenue aux étudiants-e-s 2019-2020 !")
